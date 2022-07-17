733b96b0bb5ccf6043ecfc6a8aee379e3139dcf9
YRDx-AI Lab <106952306+yrdxai@users.noreply.github.com>
2022-07-17 23:15:42 +0700

    Update Home.tsx

--- src/page/home/Home.tsx
+++ src/page/home/Home.tsx
@@ -12,19 +12,15 @@ function Home() {
           </h1>
           <div className="row">
             <Card 
-            cardLink="https://bieqa.github.io/manifold_shape.html"
             cardImg="https://bieqa.github.io/images/res_pic/img1.jpg" cardTitle="Deep Learning in Diagnosis of Lower Limb Diseases on MR Images" />
 
             <Card 
-            cardLink="https://bieqa.github.io/subcortical_shape.html"
             cardImg="https://bieqa.github.io/images/res_pic/LDDMM.png" cardTitle="Deep Learning in Brain & Breast Cancer Prediction on MR Images" />
           
             <Card 
-              cardLink="https://bieqa.github.io/Imaging_Genetics2.html"
               cardImg="	https://bieqa.github.io/images/neonatal_anat_net.jpg" cardTitle="Graph Neural Network in Tracking Diversity of Stem Cells Development" />
             
             <Card 
-              cardLink="https://bieqa.github.io/signal.html"
               cardImg="https://bieqa.github.io/images/res_pic/img4.png" cardTitle="Deep Learning in Diabetic Retinopathy Analysis" />
           </div>
         </div>
@@ -34,12 +30,10 @@ function Home() {
           </h1>
           <div className="row">
             <Card 
-            cardLink="https://bieqa.github.io/brain_development.html"
-            cardImg="https://bieqa.github.io/images/res_pic/img5.jpg" cardTitle="Brain Development GUSTO" />
+            cardImg="https://bieqa.github.io/images/res_pic/img5.jpg" cardTitle="Fully Diagnosis of ACL injuries with Multi-signals" />
 
             <Card 
-            cardLink="https://bieqa.github.io/brain_aging.html"
-            cardImg="https://bieqa.github.io/images/res_pic/subcortical_shape.jpg" cardTitle="Brain Aging" />
+            cardImg="https://bieqa.github.io/images/res_pic/subcortical_shape.jpg" cardTitle="Application of Artificial Intelligence in Rehabilitation" />
           
           </div>
         </div>
@@ -51,7 +45,7 @@ function Home() {
             <Tile 
               tileImg={hightlightProjectImage1}
               tileDescription='Automatic Detection of Meniscus Tears Using Backbone Convolutional Neural Networks on Knee MRI; Truong Nguyen Khanh Hung, Vu Pham Thao Vy, Nguyen Minh Tri, Le Ngoc Hoang, Le Van Tuan, Quang Thai Ho, Nguyen Quoc Khanh Le, Jiunn-Horng Kang; Journal of Magnetic Resonance Imaging (doi: 10.1002/jmri.28283)'
-              tileLink="https://onlinelibrary.wiley.com/journal/15222586"
+              tileLink="https://onlinelibrary.wiley.com/doi/10.1002/jmri.28284"
             />
           </div>
         </div>
