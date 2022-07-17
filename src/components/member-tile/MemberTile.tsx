7aa02c35e95802bb1520567b2c507b9fb653a9f9
Ngo Van Phat <phat-nv@blockhive.vn>
2022-07-17 18:37:12 +0700

    add our team page

---
+++ src/components/member-tile/MemberTile.tsx
@@ -0,0 +1,36 @@
+import "./MemberTile.css";
+
+export interface MemberTileProps {
+    avatar: string
+    fullName: string
+    role: string
+    descriptions: Array<string>
+}
+function MemberTile(props: MemberTileProps) {
+
+    return (
+        <div className="member-tile flex-wrap">
+            <div className="row align-start">
+                {
+                props.avatar ?
+                <img 
+                className="avatar"
+                src={props.avatar} alt="avatar" /> : null}
+                <div className="description">
+                    <h4
+                    className="member-name"
+                    >{props.fullName}</h4>
+                    <p>
+                        <b>{props.role}</b>
+                    </p>
+                    {
+                        props.descriptions.map((item: string) => <p><i>{item}</i></p>)
+                    }
+                </div>
+                
+            </div>
+        </div>
+    );
+}
+
+export default MemberTile;
--- src/page/our-team/OurTeam.tsx
+++ src/page/our-team/OurTeam.tsx
@@ -1,9 +1,81 @@
+import "./OurTeam.css"
+import MemberTile, { MemberTileProps } from "../../components/member-tile/MemberTile"
+import picture1 from "../../assets/img/le-ngoc-hoang.png"
+import picture2 from "../../assets/img/vu-pham-thao-vy.png"
+import picture3 from "../../assets/img/nguyen-minh-tri.png"
+import picture4 from "../../assets/img/ngo-van-phat.png"
+import picture5 from "../../assets/img/nguyen-thuan-phat.png"
+import picture6 from "../../assets/img/truong-nguyen-khanh-hung.png"
+
 
 function OurTeam() {
+  const members: Array<MemberTileProps> = [
+    {
+      avatar: picture1,
+      fullName: "Le Ngoc Hoang",
+      role: "Lead Reseacher",
+      descriptions: ["M.S. of College of Biomedical Engineering, Taipei Medical University", "Founder of YRDx-AI Laboratory"]
+    },
+    {
+      avatar: picture2,
+      fullName: "Dr. Vu Pham Thao Vy  ",
+      role: "Radiologist",
+      descriptions: ["M.D. of Thai Nguyen National Hospital", "Ph.D. candidate of International Ph.D. Program in Medicine, Taipei Medical University, Taipei, Taiwan", "Co-Founder of YRDx-AI Laboratory"]
+    },
+    {
+      avatar: picture3,
+      fullName: "Nguyen Minh Tri",
+      role: "Lead Technician",
+      descriptions: ["B.A. of Advance Program in Software Engineer, University of Science, Ho Chi Minh City, Vietnam", "Co-Founder of YRDx-AI Laboratory"]
+    },
+    {
+      avatar: picture4,
+      fullName: "Ngo Van Phat",
+      role: "Full stack developer",
+      descriptions: ["B.A of Advance Program in Software Engineer, University of Science, Ho Chi Minh City, Vietnam"]
+    },
+    {
+      avatar: picture5,
+      fullName: "Nguyen Thuan Phat ",
+      role: "Developer",
+      descriptions: ["B.A. of Department of Computer Science, Vietnamese German University, Ho Chi Minh City, Vietnam"]
+    },
+    {
+      avatar: "",
+      fullName: "",
+      role: "",
+      descriptions: []
+    }
+  ]
 
+  const advisors: Array<MemberTileProps> = [    
+    {
+      avatar: picture6,
+      fullName: "Dr. Truong Nguyen Khanh Hung",
+      role: "",
+      descriptions: ["M.D. of Department of Orthopedic and Trauma, Cho Ray Hospital, Ho Chi Minh City, Vietnam", "Ph.D. of International Ph.D. Program in Medicine, Taipei Medical University, Taipei, Taiwan"]
+    },
+    {
+      avatar: "",
+      fullName: "",
+      role: "",
+      descriptions: []
+    }
+  ]
   return (
     <div className="our-team">
-        <h1>Ours Team</h1>
+        <h1>Ours Members</h1>
+        <div className="row flex-wrap center">
+            {
+              members.map((member) => <MemberTile avatar={member.avatar} fullName={member.fullName} role={member.role} descriptions={member.descriptions}/>)
+            }
+        </div>
+        <h1>Ours Advisors</h1>
+        <div className="row flex-wrap center">
+            {
+              advisors.map((advisor) => <MemberTile avatar={advisor.avatar} fullName={advisor.fullName} role={advisor.role} descriptions={advisor.descriptions}/>)
+            }
+        </div>
     </div>
   )
 }
